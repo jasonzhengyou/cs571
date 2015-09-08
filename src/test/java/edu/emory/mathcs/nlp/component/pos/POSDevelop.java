ed7e0abe5e5d5c667862c371845ede403f3b4eec
meera1hahn <meera1hahn@gmail.com>
2015-09-07 22:00:34 -0400

    Merge remote-tracking branch 'emory-courses/master'
    Conflicts:
    	src/test/java/edu/emory/mathcs/nlp/component/pos/POSDevelop.java

--- src/test/java/edu/emory/mathcs/nlp/component/pos/POSDevelop.java
+++ src/test/java/edu/emory/mathcs/nlp/component/pos/POSDevelop.java
@@ -16,10 +16,12 @@
 package edu.emory.mathcs.nlp.component.pos;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.junit.Test;
 
+import edu.emory.mathcs.nlp.common.util.FileUtils;
 import edu.emory.mathcs.nlp.common.util.IOUtils;
 import edu.emory.mathcs.nlp.component.eval.AccuracyEval;
 import edu.emory.mathcs.nlp.component.eval.Eval;
@@ -39,10 +41,15 @@ public class POSDevelop
 	@Test
 	public void develop() throws IOException
 	{
+<<<<<<< HEAD
 		final String  root = "/Users/meerahahn/Desktop/GitHub/cs571/wsj-pos/";
 		//final String train_file = root+"trn/00.pos";
 		//final String  develop_file = root+"dev/20.pos";
 		final boolean average = true;
+=======
+		final String  root = "/Users/jdchoi/Documents/Data/experiments/wsj/pos/";
+		final boolean average = false;
+>>>>>>> emory-courses/master
 		final double  ambiguity_class_threshold = 0.4;
 		final double  learning_rate = 0.02;
 		final double  ridge = 0.1;
@@ -51,15 +58,21 @@ public class POSDevelop
 		final int     feature_cutoff = 2;
 		
 		TSVReader<POSNode> reader = new TSVReader<>(new POSIndex(0,1));
+		List<String> trainFiles   = FileUtils.getFileList(root+"trn/", "pos");
+		List<String> developFiles = FileUtils.getFileList(root+"dev/", "pos");
 		
 		// collect ambiguity classes from the training data
 		System.out.println("Collecting ambiguity classes.");
 		AmbiguityClassMap ambi = new AmbiguityClassMap();
+<<<<<<< HEAD
 		String train_file;
 		for (int i = 0; i < 19; i++) {
 			train_file = root+"trn/"+ String.format("%02d", i) + ".pos";
 			iterate(reader, train_file, nodes -> ambi.addAll(nodes));
 		}
+=======
+		iterate(reader, trainFiles, nodes -> ambi.add(nodes));
+>>>>>>> emory-courses/master
 		ambi.expand(ambiguity_class_threshold);
 		
 		// collect training instances from the training data
@@ -67,10 +80,14 @@ public class POSDevelop
 		StringModel model = new StringModel(new MultinomialWeightVector());
 		POSTagger<POSNode> tagger = new POSTagger<>(NLPFlag.TRAIN, model);
 		tagger.setAmbiguityClassMap(ambi);
+<<<<<<< HEAD
 		for (int i = 0; i < 19; i++) {
 			train_file = root+"trn/"+ String.format("%02d", i) + ".pos";
 			iterate(reader, train_file, nodes -> tagger.process(nodes));
 		}
+=======
+		iterate(reader, trainFiles, nodes -> tagger.process(nodes));
+>>>>>>> emory-courses/master
 		model.vectorize(label_cutoff, feature_cutoff);
 		
 		// train the statistical model using the development data
@@ -85,6 +102,7 @@ public class POSDevelop
 		{
 			sgd.train(model.getInstanceList());
 			eval.clear();
+<<<<<<< HEAD
 			String develop_file;
 			for (int j = 19; j < 22; j++) {
 				develop_file = root+"dev/"+ String.valueOf(j) + ".pos";
@@ -93,18 +111,34 @@ public class POSDevelop
 			if (eval.score() > max_eval) {
 				max_eval = eval.score();
 			}
+=======
+			iterate(reader, developFiles, nodes -> tagger.process(nodes));
+>>>>>>> emory-courses/master
 			System.out.printf("%3d: %5.2f\n", i, eval.score());
 		}
 		System.out.printf("Max eval: %5.2f\n", max_eval);
 	}
 	
-	void iterate(TSVReader<POSNode> reader, String filename, Consumer<POSNode[]> f) throws IOException
+	void iterate(TSVReader<POSNode> reader, List<String> filenames, Consumer<POSNode[]> f) throws IOException
 	{
+<<<<<<< HEAD
 		reader.open(IOUtils.createFileInputStream(filename));
 		POSNode[] nodes;	
 		while ((nodes = reader.next()) != null)
 			f.accept(nodes);
 		
 		reader.close();
+=======
+		for (String filename : filenames)
+		{
+			reader.open(IOUtils.createFileInputStream(filename));
+			POSNode[] nodes;
+			
+			while ((nodes = reader.next()) != null)
+				f.accept(nodes);
+			
+			reader.close();	
+		}
+>>>>>>> emory-courses/master
 	}
 }
