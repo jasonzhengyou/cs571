74cc4c125d800075f9d8b0fcf11db0380d261352
meera1hahn <meera1hahn@gmail.com>
2015-10-04 20:25:25 -0400

    feature engineering
    trying out a new feature template

--- src/main/java/edu/emory/mathcs/nlp/component/dep/feature/DEPFeatureTemplate1.java
+++ src/main/java/edu/emory/mathcs/nlp/component/dep/feature/DEPFeatureTemplate1.java
@@ -65,10 +65,6 @@ public class DEPFeatureTemplate1 extends DEPFeatureTemplate{
 		add(new FeatureItem<>(Source.j, 0, Field.prefix));
 		add(new FeatureItem<>(Source.i, 0, Field.prefix));
 		add(new FeatureItem<>(Source.k, 0, Field.prefix));
-		
-		add(new FeatureItem<>(Source.i, 0, Field.uncapitalized_simplified_word_form));
-		add(new FeatureItem<>(Source.j, 0, Field.uncapitalized_simplified_word_form));
-
 
 		// 2nd-order features
 		add(new FeatureItem<>(Source.i, Relation.h  , 0, Field.lemma));
@@ -107,7 +103,17 @@ public class DEPFeatureTemplate1 extends DEPFeatureTemplate{
 		add(new FeatureItem<>(Source.i, Relation.h, 0, Field.simplified_word_form));
 		add(new FeatureItem<>(Source.i, Relation.h2, 0, Field.simplified_word_form));
 
-
+//2 gram
+		//add(new FeatureItem<>(Source.i, Relation.h, 0, Field.lemma), new FeatureItem<>(Source.i, Relation.h2, 0, Field.lemma));
+		//add(new FeatureItem<>(Source.i, Relation.h, 0, Field.pos_tag), new FeatureItem<>(Source.i, Relation.h2, 0, Field.pos_tag));
+		
+		add(new FeatureItem<>(Source.k, 0, Field.simplified_word_form), new FeatureItem<>(Source.k, 1, Field.pos_tag));
+		add(new FeatureItem<>(Source.k, 0, Field.prefix), new FeatureItem<>(Source.k, 1, Field.prefix));
+		add(new FeatureItem<>(Source.k, 0, Field.suffix), new FeatureItem<>(Source.k, 1, Field.suffix));
+		add(new FeatureItem<>(Source.k, 0, Field.lemma), new FeatureItem<>(Source.k, 1, Field.lemma));
+//3 gram
+		add(new FeatureItem<>(Source.k, 0, Field.lemma), new FeatureItem<>(Source.k, 1, Field.lemma), new FeatureItem<>(Source.k, 2, Field.lemma));
+		add(new FeatureItem<>(Source.k, 0, Field.simplified_word_form), new FeatureItem<>(Source.k, 1, Field.pos_tag), new FeatureItem<>(Source.k, 2, Field.pos_tag));
 		
 		// boolean features
 		addSet(new FeatureItem<>(Source.i, 0, Field.binary));
