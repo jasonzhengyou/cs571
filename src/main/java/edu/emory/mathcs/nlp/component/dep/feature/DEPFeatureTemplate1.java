97644389a877ad453aaa87c8465026310b1f0efe
meera1hahn <meera1hahn@gmail.com>
2015-10-04 21:57:51 -0400

    feature engineering
    adding capitalized to feature template

--- src/main/java/edu/emory/mathcs/nlp/component/dep/feature/DEPFeatureTemplate1.java
+++ src/main/java/edu/emory/mathcs/nlp/component/dep/feature/DEPFeatureTemplate1.java
@@ -100,9 +100,9 @@ public class DEPFeatureTemplate1 extends DEPFeatureTemplate{
 		add(new FeatureItem<>(Source.i, Relation.rmd2, 0, Field.dependency_label));
 		add(new FeatureItem<>(Source.j, Relation.lmd2, 0, Field.dependency_label));
 		
-		add(new FeatureItem<>(Source.i, 0, Field.distance));
-		add(new FeatureItem<>(Source.j, 0, Field.distance));
-		add(new FeatureItem<>(Source.k, 0, Field.distance));
+		add(new FeatureItem<>(Source.i, 0, Field.capitalized));
+		add(new FeatureItem<>(Source.j, 0, Field.capitalized));
+		add(new FeatureItem<>(Source.k, 0, Field.capitalized));
 
 		
 		add(new FeatureItem<>(Source.i, Relation.h, 0, Field.simplified_word_form));
